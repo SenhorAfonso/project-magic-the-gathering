1a40e0333d8d10ed817bde26499873802a25dcb0
Pedro Afonso <sirafonso005896@outlook.com>
2024-09-21 11:26:15 -0300

    :sparkles: add cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,24 @@ import { UserModule } from './modules/user.module';
 import { DecksModule } from './modules/decks.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import type { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        url: configService.get<string>('REDIS_URL'),
+        ttl: 5000,
+        max: 100,
+      }),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
